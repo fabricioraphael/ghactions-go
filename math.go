3fca9f409fa9aa9490010b29113baeaddf81618b
fabricio raphael <fabricioraphael@fabricios-MacBook-Pro.local>
2021-11-20 17:14:17 -0300

    rename method Soma

--- math.go
+++ math.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(Soma(10, 15))
+	fmt.Println(soma(10, 15))
 }
 
-func Soma(first int, second int) int {
+func soma(first int, second int) int {
 	return first + second
 }
